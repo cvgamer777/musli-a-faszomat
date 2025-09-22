af8d168b885701bb398d8e4b80babcb422a88b80
Tarjanyi Levente <tarjanyi.levi@gmail.com>
2025-09-22 09:07:55 +0200

    szar ez a geci

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,21 +5,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 export default class App extends React.Component {
+  state = {
+    musliData : null
+
+  }
+  
   render() {
     return (
       <div className='page-container'>
         <nav id="navbar" title="Toggle menu width">
-          <div class="menu-item" tabindex="0" data-content="products">
+          <div className="menu-item" tabIndex="0" data-content="products">
             <span className="menu-icon">📦</span>
             <span className="menu-text">Products</span>
           </div>
 
-          <div className="menu-item" tabindex="0" data-content="prices">
+          <div className="menu-item" tabIndex="0" data-content="prices">
             <span className="menu-icon">💰</span>
             <span className="menu-text">Prices</span>
           </div>
 
-          <div className="menu-item" tabindex="0" data-content="about">
+          <div className="menu-item" tabIndex="0" data-content="about">
             <span className="menu-icon">ℹ️</span>
             <span className="menu-text">About</span>
           </div>
@@ -38,17 +43,23 @@ export default class App extends React.Component {
             </thead>
             <tbody>
               <tr><td>1</td><td>Classic Muesli</td><td>$4.65</td></tr>
-              <tr><td>2</td><td>Berry Lovers</td><td>$3.02</td></tr>
-              <tr><td>3</td><td>Nuts & Honey</td><td>$4.12</td></tr>
-              <tr><td>4</td><td>Tropical Mix</td><td>$4.93</td></tr>
-              <tr><td>5</td><td>Choco Delight</td><td>$2.73</td></tr>
-              <tr><td>6</td><td>Organic Oats</td><td>$3.55</td></tr>
-              <tr><td>7</td><td>Apple Cinnamon</td><td>$5.42</td></tr>
-              <tr><td>8</td><td>Fruit & Nut</td><td>$5.60</td></tr>
+              {}
             </tbody>
           </table>
         </main>
       </div>
     )
+  }
+  async componentDidMount() {
+    try {
+      let musliData = await fetch('http://localhost:3333/muslis')
+      console.log('musliData', musliData) 
+      musliData = await musliData.json()
+      console.log('musliData',musliData)
+    }
+    catch(e) {
+      console.warn(e)
+    }
+
   }
 }
