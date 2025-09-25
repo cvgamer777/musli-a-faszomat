3e46186a2f643caccb84ab62b8677a8ee9187e6a
Tarjanyi Levente <tarjanyi.leiv@gmail.com>
2025-09-25 11:03:16 +0200

    fasza

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -13,9 +13,41 @@ const conn = mysql.createConnection({
     database: 'test',
 })
 
+app.post('/muslis', (req,res)=>{
+    const {name,price} = req.body
+    if  (!name || price<1 ) res.sendStatus(300)
+    conn.query("INSERT INTO muslis (name,price) VALUES (?,?)", [name,price], (err,result,fields) => {
+        const insertId = result?.insertId
+
+        console.log('insertId' , result?.insertId)
+        const responseBody  = {id: insertId,...req.body}
+        res.status(201).json(responseBody)
+
+    }) 
+})
+
+app.patch('/muslis', (req,res)=>{
+    const {id} = req.body
+    const newName = 'name' in req.body.name? req.body.name : null
+    const newPrice = req.body.price?req.body.price : NaN
+    let queryStr = "UPDATE muslis SET"
+    const updates = []
+    const values = []
+    if (newName) {
+        updates.push("name=;")
+        values.push(newName)
+    }
+    if (newPrice>0) {
+        updates.push("push=?")
+        values.push(newPrice)
+    }
+    values.push(+id)
+    conn.query(`UPDATE muslis SET ${updates.join(",")} WHERE id =?`,values, [newName,newPrice,id], (err,result,fields)=>(res.status(200).json({updateId: id,newName,newPrice, ...result})))
+
+})
 
 app.get('/muslis',(req,res)=> {
-    conn.query('SELECT id, name, price FROM musli',(err,result,fields)=>{
+    conn.query('SELECT id, name, price FROM muslis',(err,result,fields)=>{
         if(err) console.warn(err)
         if (result) {
             console.log(result)
